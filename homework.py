1e4e4da0033d19b1f3770ed58fc539850157eefa
Denis Tikhonov <denistikhonov@yandex.ru>
2023-09-07 17:23:32 +0300

    Снес репозитории, форкнул и клонировал заново, перенес работу в новую папку

--- homework.py
+++ homework.py
@@ -66,10 +66,9 @@ class Running(Training):
     CALORIES_MEAN_SPEED_SHIFT: float = 1.79
 
     def get_spent_calories(self) -> float:
-        return (
-            (self.CALORIES_MEAN_SPEED_MULTIPLIER
-             * self.get_mean_speed() + self.CALORIES_MEAN_SPEED_SHIFT)
-            * self.weight / self.M_IN_KM * self.duration * self.TO_MINS)
+        return ((self.CALORIES_MEAN_SPEED_MULTIPLIER
+                * self.get_mean_speed() + self.CALORIES_MEAN_SPEED_SHIFT)
+                * self.weight / self.M_IN_KM * self.duration * self.TO_MINS)
 
 
 class SportsWalking(Training):
